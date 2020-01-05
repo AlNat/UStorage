43a9642d8b63e16d0c49d2ad3cb69d73cbd4587a
AlNat <aleksnatarov@gmail.com>
2020-01-05 18:29:34 +0300

    Первоначальная инициализация проекта
    Общий список задач

---
+++ src/main/java/dev/alnat/ustorage/UstorageApplication.java
@@ -0,0 +1,13 @@
+package dev.alnat.ustorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alnat/ustorage/UstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alnat.ustorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UstorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
