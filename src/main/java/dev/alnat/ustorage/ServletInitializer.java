ab548b35542007cb126e0935e173f2d41cf3660e
AlNat <aleksnatarov@gmail.com>
2020-01-09 00:14:39 +0300

    Добавлен класс Util
    Мелкие правки в обвязке

--- src/main/java/dev/alnat/ustorage/ServletInitializer.java
+++ src/main/java/dev/alnat/ustorage/ServletInitializer.java
@@ -3,6 +3,10 @@ package dev.alnat.ustorage;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * Необходимо для сборки классческого war-ника
+ * https://www.baeldung.com/spring-boot-servlet-initializer
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
---
+++ src/main/java/dev/alnat/ustorage/util/Util.java
@@ -0,0 +1,43 @@
+package dev.alnat.ustorage.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.Duration;
+
+/**
+ * Created by @author AlNat on 08.01.2020.
+ * Licensed by Apache License, Version 2.0
+ */
+public class Util {
+
+    /**
+     * Метод форматирования временного интервала
+     *
+     * @param duration интервал
+     * @return форматированный интервал
+     */
+    public static String formatDuration(Duration duration) {
+        long seconds = duration.getSeconds();
+        long absSeconds = Math.abs(seconds);
+        String positive = String.format(
+                "%d ч. %02d м. %02d с.",
+                absSeconds / 3600, // часы
+                (absSeconds % 3600) / 60, // минуты
+                absSeconds % 60); // секунды
+        return seconds < 0 ? "-" + positive : positive;
+    }
+
+    /**
+     * Метод получения полного текста ошибки
+     *
+     * @param e exception
+     * @return полный стек-трейс в виде строки
+     */
+    public static String getStackTrace(Exception e) {
+        final StringWriter sw = new StringWriter();
+        final PrintWriter pw = new PrintWriter(sw, true);
+        e.printStackTrace(pw);
+        return sw.getBuffer().toString();
+    }
+
+}
